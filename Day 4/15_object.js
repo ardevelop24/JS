083fc3d57d3b2c4348050a3affc80e3266520383
ardevelop24 <ardevelop24@gmail.com>
2025-01-18 15:36:08 +0530

    push

--- Day 4/15_object.js
+++ Day 4/15_object.js
@@ -14,11 +14,18 @@ const{courseInstructor: instructor }=course
 
 
 
-// console.log(courseInstructor);
-console.log(instructor);
+// console.log(courseInstructor);   //Hitesh Sir
+console.log(instructor);         //Hitesh Sir
 
 
 //apna kaam kisi aur par daalna api kehlata hain
 
+// eg restaunt mein khana kaise banege wo mera kaam nahi hain mujhe to bas order karna hain 
+
+// menu card is the docmentation
+
 // eg of api =random User me  
 // convert json format
+
+//api array ki format mein bhi milti hain
+// randomuserme api
